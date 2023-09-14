0b2925b373af3bc3da62ff4ddd2de8a4ad805969
Nurettin KAYNAR <nurettin.kaynar@ardgrup.com.tr>
2023-09-14 13:47:55 +0300

    navbar ve hero section responsive düzeltmeleri yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} f
 const App=()=> {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      <Navbar/>
-      <Hero/>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
-    <About/>
-    <Experience/>
-    <Tech/>
-    <Works/>
-    <Feedbacks/>
-    <div className="relative z-0">
-      <Contact/>
-      <StarsCanvas/>
-    </div>
-    </div>
-    </BrowserRouter>
+  </BrowserRouter>
   )
 }
 
