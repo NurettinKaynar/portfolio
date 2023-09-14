23afd4fd1e3cf8f30819d1ee8e97f5f42217a17f
Nurettin KAYNAR <nurettin.kaynar@ardgrup.com.tr>
2023-09-14 11:51:34 +0300

    animate loader eklendi, scroll eklendi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
 
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`} >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"  />
-          <div className="w-1 sm:h-80 h-40 violet-gradient"  />
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
         </div>
       <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
@@ -22,6 +22,21 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas/>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" >
+        <a href="#about" >
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2" >
+            <motion.dev 
+            animate={{y:[0,24,0]}}
+            transition={{
+              duration:1.5,
+              repeat:Infinity,
+              repeatType:'loop'
+            }}
+            className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
